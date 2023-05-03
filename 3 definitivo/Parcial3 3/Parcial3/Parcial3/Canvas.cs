5a1b1d106c9f84a286a3565adf7069fddbd37e98
Vladimir Osmar Chavez Ramirez <vladimir.chavezrz@udlap.mx>
2023-05-03 15:39:14 -0600

    FINAL

--- 3 definitivo/Parcial3 3/Parcial3/Parcial3/Canvas.cs
+++ 3 definitivo/Parcial3 3/Parcial3/Parcial3/Canvas.cs
@@ -202,7 +202,210 @@ namespace Parcial3
                 }
             }
         }
-       
+        public void DrawShadedTriangleOH(Point p0, float h0, Point p1, float h1, Point p2, float h2, Color c)
+        {
+            List<float> x_left;
+            List<float> x_right;
+            List<float> h_left;
+            List<float> h_right;
+
+            if (p1.Y < p0.Y)
+            {
+                Point p = p0;
+                p0 = p1;
+                p1 = p;
+                float t = h0;
+                h0 = h1;
+                h1 = t;
+            }
+            if (p2.Y < p0.Y)
+            {
+                Point p = p0;
+                p0 = p2;
+                p2 = p;
+                float t = h0;
+                h0 = h2;
+                h2 = t;
+            }
+            if (p2.Y < p1.Y)
+            {
+                Point p = p2;
+                p2 = p1;
+                p1 = p;
+                float t = h2;
+                h2 = h1;
+                h1 = t;
+            }
+
+            List<float> x01 = Interpolate(p0.Y, p0.X, p1.Y, p1.X);
+            List<float> x12 = Interpolate(p1.Y, p1.X, p2.Y, p2.X);
+            List<float> x02 = Interpolate(p0.Y, p0.X, p2.Y, p2.X);
+
+            List<float> h01 = Interpolate(p0.Y, h0, p1.Y, h1);
+            List<float> h12 = Interpolate(p1.Y, h1, p2.Y, h2);
+            List<float> h02 = Interpolate(p0.Y, h0, p2.Y, h2);
+
+            x01.RemoveAt(x01.Count - 1);
+            List<float> x012 = new List<float>();
+            x012.AddRange(x01);
+            x012.AddRange(x12);
+
+            h01.RemoveAt(h01.Count - 1);
+            List<float> h012 = new List<float>();
+            h012.AddRange(h01);
+            h012.AddRange(h12);
+
+            int m = x02.Count / 2;
+            if (x02[m] < x012[m])
+            {
+                x_left = x02;
+                x_right = x012;
+
+                h_left = h02;
+                h_right = h012;
+            }
+            else
+            {
+                x_left = x012;
+                x_right = x02;
+
+                h_left = h012;
+                h_right = h02;
+            }
+
+            for (int y = p0.Y; y < p2.Y; y++)
+            {
+                float x_l = x_left[y - p0.Y];
+                float x_r = x_right[y - p0.Y];
+                List<float> h_segment = Interpolate(x_l, h_left[y - p0.Y], x_r, h_right[y - p0.Y]);
+                for (float x = x_left[y - p0.Y]; x < x_right[y - p0.Y]; x++)
+                {
+                    Color shaded_color = Color.FromArgb((int)(c.R * h_segment[(int)(x - x_l)]), (int)(c.G * h_segment[(int)(x - x_l)]), (int)(c.B * h_segment[(int)(x - x_l)]));
+                    DrawPixel((int)x, y, shaded_color);
+                }
+            }
+
+        }
+        public void DrawShadedTriangle(Point p0, Color c0, Point p1, Color c1, Point p2, Color c2)
+        {
+            List<float> x_left;
+            List<float> x_right;
+            List<float> r_left;
+            List<float> r_right;
+            List<float> g_left;
+            List<float> g_right;
+            List<float> b_left;
+            List<float> b_right;
+
+            if (p1.Y < p0.Y)
+            {
+                Point p = p0;
+                p0 = p1;
+                p1 = p;
+                Color c = c0;
+                c0 = c1;
+                c1 = c;
+            }
+            if (p2.Y < p0.Y)
+            {
+                Point p = p0;
+                p0 = p2;
+                p2 = p;
+                Color c = c0;
+                c0 = c2;
+                c2 = c;
+            }
+            if (p2.Y < p1.Y)
+            {
+                Point p = p2;
+                p2 = p1;
+                p1 = p;
+                Color c = c2;
+                c2 = c1;
+                c1 = c;
+            }
+
+            List<float> x01 = Interpolate(p0.Y, p0.X, p1.Y, p1.X);
+            List<float> x12 = Interpolate(p1.Y, p1.X, p2.Y, p2.X);
+            List<float> x02 = Interpolate(p0.Y, p0.X, p2.Y, p2.X);
+
+            List<float> r01 = Interpolate(p0.Y, c0.R, p1.Y, c1.R);
+            List<float> r12 = Interpolate(p1.Y, c1.R, p2.Y, c2.R);
+            List<float> r02 = Interpolate(p0.Y, c0.R, p2.Y, c2.R);
+
+            List<float> g01 = Interpolate(p0.Y, c0.G, p1.Y, c1.G);
+            List<float> g12 = Interpolate(p1.Y, c1.G, p2.Y, c2.G);
+            List<float> g02 = Interpolate(p0.Y, c0.G, p2.Y, c2.G);
+
+            List<float> b01 = Interpolate(p0.Y, c0.B, p1.Y, c1.B);
+            List<float> b12 = Interpolate(p1.Y, c1.B, p2.Y, c2.B);
+            List<float> b02 = Interpolate(p0.Y, c0.B, p2.Y, c2.B);
+
+            x01.RemoveAt(x01.Count - 1);
+            List<float> x012 = new List<float>();
+            x012.AddRange(x01);
+            x012.AddRange(x12);
+
+            r01.RemoveAt(r01.Count - 1);
+            List<float> r012 = new List<float>();
+            r012.AddRange(r01);
+            r012.AddRange(r12);
+
+            g01.RemoveAt(g01.Count - 1);
+            List<float> g012 = new List<float>();
+            g012.AddRange(g01);
+            g012.AddRange(g12);
+
+            b01.RemoveAt(b01.Count - 1);
+            List<float> b012 = new List<float>();
+            b012.AddRange(b01);
+            b012.AddRange(b12);
+
+            int m = x02.Count / 2;
+            if (x02[m] < x012[m])
+            {
+                x_left = x02;
+                x_right = x012;
+
+                r_left = r02;
+                r_right = r012;
+
+                g_left = g02;
+                g_right = g012;
+
+                b_left = b02;
+                b_right = b012;
+            }
+            else
+            {
+                x_left = x012;
+                x_right = x02;
+
+                r_left = r012;
+                r_right = r02;
+
+                g_left = g012;
+                g_right = g02;
+
+                b_left = b012;
+                b_right = b02;
+            }
+
+            for (int y = p0.Y; y < p2.Y; y++)
+            {
+                float x_l = x_left[y - p0.Y];
+                float x_r = x_right[y - p0.Y];
+                List<float> r_segment = Interpolate(x_l, r_left[y - p0.Y], x_r, r_right[y - p0.Y]);
+                List<float> g_segment = Interpolate(x_l, g_left[y - p0.Y], x_r, g_right[y - p0.Y]);
+                List<float> b_segment = Interpolate(x_l, b_left[y - p0.Y], x_r, b_right[y - p0.Y]);
+                for (float x = x_left[y - p0.Y]; x < x_right[y - p0.Y]; x++)
+                {
+                    Color shaded_color = Color.FromArgb((int)(r_segment[(int)(x - x_l)]), (int)(g_segment[(int)(x - x_l)]), (int)(b_segment[(int)(x - x_l)]));
+                    DrawPixel((int)x, y, shaded_color);
+                }
+            }
+
+        }
         public static PointF TranslateToO(PointF p)
         {
             p.X = p.X + Width / 2;
@@ -212,6 +415,12 @@ namespace Parcial3
         public void RenderTriangle(Triangulo triangle, List<PointF> projected, List<Vertex> v, Boolean f)
         {
 
+            if (f)
+            {
+                DrawShadedTriangle(Point.Round(projected[triangle.p0]), v[triangle.p0].C,
+                Point.Round(projected[triangle.p1]), v[triangle.p1].C,
+                Point.Round(projected[triangle.p2]), v[triangle.p2].C);
+            }
             DrawWireFrameTriangle(Point.Round(projected[triangle.p0]),
                 Point.Round(projected[triangle.p1]),
                 Point.Round(projected[triangle.p2]),
--- 3 definitivo/Parcial3 3/Parcial3/Parcial3/Form1.cs
+++ 3 definitivo/Parcial3 3/Parcial3/Parcial3/Form1.cs
@@ -15,11 +15,19 @@ namespace Parcial3
     {
          public Canvas canvas;
         PictureBox pictureBox;
-        TrackBar cd,cr1,cr2, cr;
+        TrackBar cd,cr1,cr2, cr,movex,movey;
+        Button grabar,play, parar,fill;
         Scene scene = new Scene();
-        Model cubo;
-        Transform c;
+        Scene scene2 = new Scene();
+        Scene scene3 = new Scene();
+        Model cubo,cube,cubaibi;
+        Transform c,c2;
         int w, h;
+        float posicionx1,posicionxfinal1,posiciony1,posicionyfinal1;
+        float tamañoinicio1, tamañofinal1;
+        float rotacionx1, rotacionfinalx1;
+        float rotaciony1, rotacionfinaly1;
+        float rotacionz1, rotacionfinalz1;
         public static float v_H = 1f, v_W, c_H, c_W;
         public static float distance = 1;
         Boolean fll = false;
@@ -30,27 +38,47 @@ namespace Parcial3
             InitializeComponent();
             this.BackColor = Color.Orange;
             AddOBJ();
-            scene.instance.Add(new Instance(cubo, new Transform(20, new Vertex(0, 0, 0), new Vertex(0, 0, 0))));
+            AddOBJ2();
+            AddOBJ3();
+            scene.instance.Add(new Instance(cubo, new Transform(3, new Vertex(0, 0, 0), new Vertex(0, 0, 0))));
+            scene2.instance.Add(new Instance(cube, new Transform(3, new Vertex(0, 0, 0), new Vertex(-6, 0, 0))));
+            scene3.instance.Add(new Instance(cubaibi, new Transform(3, new Vertex(0, 0, 0), new Vertex(-10, 0, 0))));
            
         }
-         string path = "C:\\Users\\vlady\\Downloads\\Gun.obj";
+         string path = "Gun.obj";
+        string path2 = "Gun1.obj";
+        string path3 = "Gun2.obj";
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             canvas.FastClear();
             
             scene.instance[0].transform.X = 0;
+            scene2.instance[0].transform.X = 0;
+            scene3.instance[0].transform.X = 0;
+           
+   
+
             
             c.ZPosition = -cd.Value;
             c.Y = -cr2.Value;
             c.X = -cr1.Value;
             c.Z = -cr.Value;
 
+            c.XPosition = movex.Value;
+            c.YPosition = movey.Value;
+
+
+
             canvas.RenderScene(scene, fll, c);
+            canvas.RenderScene(scene2, fll, c);
+            canvas.RenderScene(scene3, fll, c);
             pictureBox.Invalidate();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            timer2.Interval = 80; // se dispara cada segundo (1000 milisegundos)
+            timer2.Enabled = false;
             w = Form1.ActiveForm.Width - 90;
             h = Form1.ActiveForm.Height - 250;
             c_W = w;
@@ -58,13 +86,21 @@ namespace Parcial3
             v_W = (float)w / (float)h;
             canvas = new Canvas(new Size(w, h));
             AddControls();
-            this.Controls.Add(pictureBox);
+            
             this.Controls.Add(cd);
             this.Controls.Add(cr);
             this.Controls.Add(cr1);
             this.Controls.Add(cr2);
+            this.Controls.Add(movex);
+            this.Controls.Add(movey);           
+            this.Controls.Add(grabar);
+            this.Controls.Add(fill);
+            this.Controls.Add(play);
+            this.Controls.Add(parar);
+            this.Controls.Add(pictureBox);
             c = new Transform(1, new Vertex(0, 0, 0), new Vertex(0, 0, 0));
             c.FOV = 110;
+           
         }
       
 
@@ -102,7 +138,188 @@ namespace Parcial3
             
         }
 
+         private void AddOBJ2()
+        {
+            string[] lines = System.IO.File.ReadAllLines(path2), tex, f;
+            int[] tri = new int[3];
+            List<Vertex> v = new List<Vertex>();
+            List<Triangulo> t = new List<Triangulo>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] != "")
+                {
+                    if (lines[i][0] == 'v')
+                    {
+                        if (lines[i][1] == ' ')
+                        {
+                            tex = lines[i].Split(' ');
+                            v.Add(new Vertex(float.Parse(tex[1]), float.Parse(tex[2]), float.Parse(tex[3])));
+                        }
+                    }
+                    if (lines[i][0] == 'f')
+                    {
+                        tex = lines[i].Split(' ');
+                        for (int j = 1; j < 4; j++)
+                        {
+                            f = tex[j].Split('/');
+                            tri[j - 1] = int.Parse(f[0]) - 1;
+                        }
+                        t.Add(new Triangulo(tri[0], tri[1], tri[2], Color.Purple));
+                    }
+                }
+            }
+            cube = new Model(v, t);
+            
+        }
+
+
+        private void AddOBJ3()
+        {
+            string[] lines = System.IO.File.ReadAllLines(path3), tex, f;
+            int[] tri = new int[3];
+            List<Vertex> v = new List<Vertex>();
+            List<Triangulo> t = new List<Triangulo>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] != "")
+                {
+                    if (lines[i][0] == 'v')
+                    {
+                        if (lines[i][1] == ' ')
+                        {
+                            tex = lines[i].Split(' ');
+                            v.Add(new Vertex(float.Parse(tex[1]), float.Parse(tex[2]), float.Parse(tex[3])));
+                        }
+                    }
+                    if (lines[i][0] == 'f')
+                    {
+                        tex = lines[i].Split(' ');
+                        for (int j = 1; j < 4; j++)
+                        {
+                            f = tex[j].Split('/');
+                            tri[j - 1] = int.Parse(f[0]) - 1;
+                        }
+                        t.Add(new Triangulo(tri[0], tri[1], tri[2], Color.Purple));
+                    }
+                }
+            }
+            cubaibi = new Model(v, t);
+            
+        }
+
+        private void grabar1_Click(object sender, EventArgs e)
+        {
+          posicionx1 = c.XPosition;
+          posiciony1 = c.YPosition;
+          tamañoinicio1 = cd.Value;
+          rotacionx1 = cr1.Value;
+          rotaciony1 = cr2.Value;
+          rotacionz1 = cr.Value;
+        }
+        private void parar1_Click(object sender, EventArgs e)
+        {
+          posicionxfinal1 = c.XPosition;
+          posicionyfinal1 = c.YPosition;
+          tamañofinal1 = cd.Value;
+          rotacionfinalx1 = cr1.Value;
+          rotacionfinaly1 = cr2.Value;
+          rotacionfinalz1 = cr.Value;
+     
+        }
        
+        private void play_Click(object sender, EventArgs e)
+        {
+          timer2.Enabled = true;
+        }
+
+        Timer timer2 = new Timer();
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            canvas.FastClear();
+            if (posicionx1 <= posicionxfinal1)
+            {
+                posicionx1 ++;
+                c.XPosition = posicionx1;
+               
+            }
+            if (posiciony1 <= posicionyfinal1)
+            {
+                posiciony1 ++;
+                c.YPosition = posiciony1;
+                
+            }
+             if (tamañoinicio1 >= -13 && tamañoinicio1 <= tamañofinal1)
+            {
+                tamañoinicio1 ++;
+                c.ZPosition = tamañoinicio1;
+               
+            }
+             if (tamañoinicio1 <= -13 && tamañoinicio1 >= tamañofinal1)
+            {
+                tamañoinicio1 --;
+                c.ZPosition = tamañoinicio1;
+              
+            }
+              if (rotacionx1>= 0 && rotacionx1 <= rotacionfinalx1)
+            {
+                rotacionx1 +=5;
+                c.X = rotacionx1;
+              
+            }
+              if (rotacionx1 <= 0 && rotacionx1 >= rotacionfinalx1)
+            {
+                rotacionx1 -=5;
+                c.X = rotacionx1;
+              
+            }
+              if (rotaciony1>= 0 && rotaciony1 <= rotacionfinaly1)
+            {
+                rotaciony1 +=5;
+                c.Y = rotaciony1;
+              
+            }
+              if (rotaciony1 <= 0 && rotaciony1 >= rotacionfinaly1)
+            {
+                rotaciony1 -=5;
+                c.Y = rotaciony1;
+              
+            }
+              if (rotacionz1>= 0 && rotacionz1 <= rotacionfinalz1)
+            {
+                rotacionz1 +=5;
+                c.Z = rotacionz1;
+              
+            }
+              if (rotacionz1 <= 0 && rotacionz1 >= rotacionfinalz1)
+            {
+                rotacionz1 -=5;
+                c.Z = rotacionz1;
+              
+            }
+           
+            canvas.RenderScene(scene, fll, c);
+            canvas.RenderScene(scene2, fll, c);
+            canvas.RenderScene(scene3, fll, c);
+            pictureBox.Invalidate();
+        }
+        private void FILL_Click(object sender, EventArgs e)
+        {
+            fll = !fll;
+            if (fll)
+            {
+                fill.Text = "WireFrame";
+                fll = true;
+                pictureBox.Invalidate();
+
+            } else
+            {
+                fill.Text = "Texture";
+            }
+        }
+
+
+        
         private void AddControls()
         {
             pictureBox = new PictureBox
@@ -115,7 +332,7 @@ namespace Parcial3
             
             cd = new TrackBar
             {
-                Location = new Point(w + 5, 50),
+                Location = new Point(w + 30, 75),
                 Orientation = Orientation.Vertical,
                 Height = h,
                 LargeChange = 1,
@@ -127,7 +344,7 @@ namespace Parcial3
             
             cr = new TrackBar
             {
-                Location = new Point(0, h + 50),
+                Location = new Point(0, h + 75),
                 Width = w,
                 Maximum = 180,
                 LargeChange = 1,
@@ -137,7 +354,7 @@ namespace Parcial3
             };
              cr1 = new TrackBar
             {
-                Location = new Point(0, h + 100),
+                Location = new Point(0, h + 125),
                 Width = w,
                 Maximum = 180,
                 LargeChange = 1,
@@ -147,7 +364,7 @@ namespace Parcial3
             };
              cr2 = new TrackBar
             {
-                Location = new Point(0, h + 150),
+                Location = new Point(0, h + 175),
                 Width = w,
                 Maximum = 180,
                 LargeChange = 1,
@@ -155,7 +372,66 @@ namespace Parcial3
                 Value = 0,
                 BackColor = Color.Orange
             };
+
+             movex = new TrackBar
+            {
+                Location = new Point(0,0),
+                Width = w-150,
+                Maximum = 300,
+                LargeChange = 1,
+                Minimum = -300,
+                Value = 0,
+                BackColor = Color.Orange,
+                
+            };
+            movey = new TrackBar
+            {
+                Location = new Point(0,45),
+                Width = w-150,
+                Maximum = 300,
+                LargeChange = 1,
+                Minimum = -300,
+                Value = 0,
+                BackColor = Color.Orange
+            };
             
+            
+            grabar = new Button
+            {
+                Location = new Point(w-140,0),
+                BackColor = Color.Gray,
+                Text = "GRABAR1",
+                ForeColor = Color.Black
+            };
+
+             play = new Button
+            {
+                Location = new Point(w,45),
+                BackColor = Color.Gray,
+                Text = "PLAY",
+                ForeColor = Color.Black
+            };
+            parar = new Button
+            {
+                Location = new Point(w-140,22),
+                BackColor = Color.Gray,
+                Text = "PARAR1",
+                ForeColor = Color.Black
+            };
+            fill = new Button
+            {
+                Location = new Point(w , 18),
+                Text = "Texture",
+            };
+            fill.Click += FILL_Click;
+             
+            grabar.Click += new EventHandler(grabar1_Click);
+            parar.Click += new EventHandler(parar1_Click);
+            play.Click += new EventHandler(play_Click);
+            timer2.Tick += new EventHandler(timer2_Tick);
+
+            
+                      
 
         }
         
